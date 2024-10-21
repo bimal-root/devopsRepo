42bf54b84e08fdb16292218738614c862e8dfa22
Bimalraj Sharma <60562137+bimal-root@users.noreply.github.com>
2024-10-21 20:28:26 +1100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull bimalrajsharma07/simple-python-flask-app:latest
+docker pull bimalrajsharma07/simple-python-flask-app
 
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 bimalrajsharma07/simple-python-flask-app:latest
+docker run -d -p 5000:5000 bimalrajsharma07/simple-python-flask-app
